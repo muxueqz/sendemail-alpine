ee41087b117d15c9732e00de796f37c06afeb129
muxueqz <zhangmingyuan240@gmail.com>
2021-01-19 14:31:56 +0800

    convert to github docker actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "
 
 # The mail server (where the mail is sent to), both port 465 or 587 should be acceptable
 # See also https://support.google.com/mail/answer/78799
-mailhub=$SMTP_SERVER
+mailhub=$INPUT_SMTP_SERVER
 
 # The address where the mail appears to come from for user authentication.
 #rewriteDomain=gmail.com
@@ -18,14 +18,14 @@ UseTLS=Yes
 UseSTARTTLS=Yes
 
 # Username/Password
-AuthUser=$SMTP_USER
-AuthPass=$SMTP_PASSWORD
+AuthUser=$INPUT_SMTP_USER
+AuthPass=$INPUT_SMTP_PASSWORD
 AuthMethod=LOGIN
 
 # Email 'From header's can override the default domain?
 FromLineOverride=yes
 " > /etc/ssmtp/ssmtp.conf
 
-echo ${ATTACHMENTS} > /dev/shm/atts.txt
-echo -e "To:$TO_ADDRESS\nFrom:$FROM_ADDRESS\nSubject: $SUBJECT\n\n $BODY"| (cat - && uuencode /dev/shm/atts.txt $ATTACHMENTS_FILENAME) | \
-  ssmtp -v $TO_ADDRESS
+echo ${INPUT_ATTACHMENTS} > /dev/shm/atts.txt
+echo -e "To:$INPUT_TO_ADDRESS\nFrom:$INPUT_FROM_ADDRESS\nSubject: $INPUT_SUBJECT\n\n $INPUT_BODY"| (cat - && uuencode /dev/shm/atts.txt $INPUT_ATTACHMENTS_FILENAME) | \
+  ssmtp -v $INPUT_TO_ADDRESS
