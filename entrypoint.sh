ef95f92890c20e372113ca3c60100b991efa4ffd
muxueqz <zhangmingyuan240@gmail.com>
2021-01-20 09:32:15 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -z $INPUT_ATTACHMENTS ] ; then
+if [ -z "$INPUT_ATTACHMENTS" ] ; then
   echo  "INPUT_ATTACHMENTS is NULL!!!"
   exit 1
 fi
@@ -30,6 +30,6 @@ AuthMethod=LOGIN
 FromLineOverride=yes
 " > /etc/ssmtp/ssmtp.conf
 
-echo ${INPUT_ATTACHMENTS} > /dev/shm/atts.txt
+echo "$INPUT_ATTACHMENTS" > /dev/shm/atts.txt
 echo -e "To:$INPUT_TO_ADDRESS\nFrom:$INPUT_FROM_ADDRESS\nSubject: $INPUT_SUBJECT\n\n $INPUT_BODY"| (cat - && uuencode /dev/shm/atts.txt $INPUT_ATTACHMENTS_FILENAME) | \
   ssmtp -v $INPUT_TO_ADDRESS
